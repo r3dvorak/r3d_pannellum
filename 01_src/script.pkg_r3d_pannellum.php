b23f74a0feb6a60b19dfba78504661855c3e8871
Richard Dvořák <dev@r3d.de>
2026-08-27 22:02:23 +0200

    v5.3.3 complete backend option translations

--- 01_src/script.pkg_r3d_pannellum.php
+++ 01_src/script.pkg_r3d_pannellum.php
@@ -6,7 +6,7 @@
  * @author      Richard Dvorak, r3d.de
  * @copyright   Copyright (C) 2025 Richard Dvorak, https://r3d.de
  * @license     GPL-3.0-or-later https://www.gnu.org/licenses/gpl-3.0.html
- * @version     5.3.2
+ * @version     5.3.3
  */
 
 defined('_JEXEC') or die;
@@ -99,5 +99,6 @@ final class pkg_r3d_pannellumInstallerScript
         return true;
     }
 }
+
 
 
